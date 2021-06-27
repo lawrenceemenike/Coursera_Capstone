79351ad63c3bd6662d2ff9482f221e79bf1c3d8e
Lawrence Emenike <65117830+swerzd@users.noreply.github.com>
2021-06-27 22:04:36 +0100

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook is for the Coursera IBM Capstone Project
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[7]:
+
+
+print('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
